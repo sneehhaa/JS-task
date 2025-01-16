c7c7a6f6efeae7bb16ed7627238e3de367bce6f5
sneehhaa <sneha@smalljoys.life>
2025-01-16 11:15:55 +0530

    Round off the average

--- studentScores.js
+++ studentScores.js
@@ -2,10 +2,10 @@ const students = require('./students.json');
 
 function totalScores(scores) {
     let totalScore = 0;
-for(let i = 0; i < scores.length; i++) {
-    totalScore += scores[i];
-}
-return totalScore;
+    for(let i = 0; i < scores.length; i++) {
+        totalScore += scores[i];
+    }
+    return totalScore;
 }
 
 function average(totalScore, NOFSubjects) {
@@ -13,13 +13,11 @@ function average(totalScore, NOFSubjects) {
 }
 
 function grading(average) {
-    if(average >= 90 && average <= 100) {
+    if(average >= 90 && average <= 100) { 
         return "A";
-    }
-    if(average >= 75 && average < 90) {
+    } else if(average >= 75 && average < 90) {
         return "B";
-    }
-    if(average >= 50 && average < 75) {
+    } else if(average >= 50 && average < 75) {
         return "C";
     }
     return "F";
@@ -28,12 +26,22 @@ function grading(average) {
 for(let i = 0; i < students.length; i++) {
     const student = students [i];
     const totalScore = totalScores(student.scores)
-    const avg = average(totalScore, student.scores.length)
+    const avg = Math.round(average(totalScore, student.scores.length))
     const grade = grading(avg)
 
     console.log(`Name: ${student.name}`);
+    console.log(`Individual Scores: 10, 20, 30`);
     console.log(`TotalScore: ${totalScore}`);
+    console.log(`Pass/Fail: P F`); // min score for each subject to pass is 35
     console.log(`Average: ${avg}`);
     console.log(`Grading: ${grade}`);
     console.log("------------");
 }
+
+
+// Report:
+
+// A: 
+// B: 
+// C: 
+// F: 
