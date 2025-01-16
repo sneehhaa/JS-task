3a9fb8c9858c0fca529e9096fc37b859df8182f2
sneehhaa <sneha@smalljoys.life>
2025-01-16 15:20:15 +0530

    changing grading policy

--- studentScores.js
+++ studentScores.js
@@ -12,7 +12,10 @@ function average(totalScore, NOFSubjects) {
     return(totalScore / NOFSubjects);
 };
 
-function grading(average) {
+function grading(average, isPass) {
+    if(isPass == "Fail") {
+        return "F";
+    }
     if(average >= 90 && average <= 100) { 
         return "A";
     } else if(average >= 75 && average < 90) {
@@ -23,13 +26,26 @@ function grading(average) {
     return "F";
 };
 
-const passorfail = (scores => {
+const passOrFail = (scores => {
+    // scores.every((item)=> item >= 35) ? "Pass" : "Fail";
+    // scores.some((item)=> item < 35) ? "Fail" : "Pass";
     for(i = 0; i < scores.length; i++) {
         if(scores[i] < 35) {
             return "Fail";
         }
     }
     return "Pass";
+});
+
+const getIndScores = (scores => {
+    let score = '';
+    for(i = 0; i < scores.length; i++) {
+        score += scores[i];
+        if (i != scores.length - 1) {
+            score += ', ';
+        }
+    }
+    return score;
 });
 
 let gradesCount = {
@@ -43,15 +59,16 @@ for(let i = 0; i < students.length; i++) {
     const student = students [i];
     const totalScore = totalScores(student.scores)
     const avg = Math.round(average(totalScore, student.scores.length))
-    const PF = passorfail(student.scores)
-    const grade = grading(avg)
-    gradesCount[grade]++;
+    const isPass = passOrFail(student.scores)
+    const grade = grading(avg, isPass)
     
+    gradesCount[grade]++;
 
     console.log(`Name: ${student.name}`);
-    console.log(`Individual Scores: ${student.scores.join()}`);
+    // console.log(`Individual Scores: ${student.scores.join()}`);
+    console.log(`Individual Scores: ${getIndScores(student.scores)}`);
     console.log(`TotalScore: ${totalScore}`);
-    console.log(`Pass/Fail: ${PF}`); // min score for each subject to pass is 35
+    console.log(`Pass/Fail: ${isPass}`); // min score for each subject to pass is 35
     console.log(`Average: ${avg}`);
     console.log(`Grading: ${grade}`);
     console.log("------------");
